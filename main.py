6daa6dbff0b8cc75058d7b19079d3fe5666fdca4
wjsgotnr73-collab <wjsgotnr73@gmail.com>
2026-06-19 05:35:55 +0000

    smoking

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import os
 import requests
 from dotenv import load_dotenv
 
-# 1. .env 파일에 숨겨둔 API 키 불러오기
 load_dotenv()
 API_KEY = os.environ.get("MY_API_KEY")
 
@@ -10,11 +9,9 @@ def get_busan_smoking_areas():
     print("🚬 부산광역시 흡연구역 통합 탐색을 시작합니다...\n")
     print("=" * 50)
 
-    # 2. 여러 구의 데이터를 다루기 위해 리스트와 딕셔너리 활용!
     districts = [
         {
             "name": "서구", 
-            # ⚠️ 현재 서구에도 연제구 주소가 들어가 있습니다. 나중에 서구 진짜 주소로 바꿔주세요!
             "url": "https://api.odcloud.kr/api/15029124/v1/uddi:0a174c18-7056-460d-97d4-91abae89f6a3" 
         },
         {
@@ -23,13 +20,12 @@ def get_busan_smoking_areas():
         }
     ]
     
-    # 3. 리스트를 돌면서 각 구의 데이터를 순서대로 가져옴 (반복문)
     for district in districts:
-        print(f"🔎 [{district['name']}] 데이터를 서버에 요청합니다...")
+        print(f"🔎 [{district['name']}] 데이터를 서버에 요청합니다...\n")
         
         params = {
             'page': 1,
-            'perPage': 5,            # 테스트용으로 각 구마다 5개씩만 뽑아봅시다
+            'perPage': 5,          
             'serviceKey': API_KEY,
             'returnType': 'JSON'
         }
@@ -42,21 +38,20 @@ def get_busan_smoking_areas():
                 areas = data.get('data', [])
                 
                 if not areas:
-                    print(f"  -> 텅 비었네요. {district['name']} URL을 다시 확인해주세요.\n")
-                    continue # 다음 구로 넘어감
+                    print(f"  -> 텅 비었네요... {district['name']} URL을 다시 확인해주세요.\n")
+                    continue 
 
-                print(f"  ✅ 성공! {district['name']}에서 {len(areas)}개의 흡연구역 발견!\n")
+                print(f"  ✅  성공! {district['name']}에서 {len(areas)}개의 흡연구역 발견!\n")
                 
-                # 가져온 데이터 예쁘게 출력
+            
                 for index, area in enumerate(areas, start=1):
-                    # 💡 원본 데이터 확인용 코드 (정확한 띄어쓰기 적용)
-                    print(f"원본 데이터 확인: {area}") 
+                    print(f"원본 데이터 확인: {area}\n") 
                     
                     location = area.get('시설명', area.get('설치위치', area.get('장소명', '위치 정보 없음')))
                     address = area.get('주소', area.get('소재지도로명주소', area.get('소재지지번주소', '주소 정보 없음')))
                     
                     print(f"  [{district['name']} {index}] 🏢 {location}")
-                    print(f"      👉 {address}")
+                    print(f"      → {address}\n")
                 
                 print("-" * 50)
                     
